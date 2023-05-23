d85b222830df4d2e9c4575021ff1662e0884aa87
TranThanhHai1973 <tranhai11082001@gmail.com>
2023-05-23 20:46:15 +0700

    Cấu hình SASS

---
+++ src/Componments/GlobalStyles/index.js
@@ -0,0 +1,11 @@
+import './GlobalStyles.scss';
+import App from '~/App';
+function GlobalStyles({ childer }) {
+    return (
+        <div>
+            <App />
+        </div>
+    );
+}
+
+export default GlobalStyles;
