f1ec730ac9ee1bef1aa9072d09da7da74c4f7357
TranThanhHai1973 <tranhai11082001@gmail.com>
2023-05-23 20:45:47 +0700

    Cấu hình SASS

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
-import Button from './Componments/Button';
 function App() {
-  return (
-    <div className="App">
-      <Button></Button>
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1>hello</h1>
+        </div>
+    );
 }
 
 export default App;
