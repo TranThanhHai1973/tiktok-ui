ab7085d8b4aec85346bfc04df4625112c2d0a490
TranThanhHai1973 <tranhai11082001@gmail.com>
2023-06-03 21:40:33 +0700

    Xây dựng UI phần Header #6

--- src/Componments/Popper/Menu/MenuItem.js
+++ src/Componments/Popper/Menu/MenuItem.js
@@ -3,9 +3,12 @@ import classNames from 'classnames/bind';
 import styles from './Menu.module.scss';
 
 const cx = classNames.bind(styles);
-function MenuItem({ data }) {
+function MenuItem({ data, onClick }) {
+    const classes = cx('menu-item', {
+        separate: data.separate,
+    });
     return (
-        <Button leftIcon={data.icon} className={cx('menu-item')}>
+        <Button leftIcon={data.icon} onClick={onClick} href={data.to} className={classes}>
             {data.title}
         </Button>
     );
